09410ffedef063b29b005e57fca8666999c15e19
William Gadney <gadnex@gmail.com>
2024-11-26 18:22:39 +0100

    Enable Swagger UI from Docker container

--- src/main/java/net/binarypaper/anemic_api/Application.java
+++ src/main/java/net/binarypaper/anemic_api/Application.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -22,7 +23,8 @@ import org.springframework.retry.annotation.EnableRetry;
             version = "${build.version}",
             contact =
                 @Contact(name = "${build.developer.name}", email = "${build.developer.email}"),
-            license = @License(name = "${build.license.name}", url = "${build.license.url}")))
+            license = @License(name = "${build.license.name}", url = "${build.license.url}")),
+    servers = @Server(url = "${application.api.endpoint}"))
 @EnableRetry
 public class Application {
 
